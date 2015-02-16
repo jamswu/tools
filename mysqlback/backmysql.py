53ccb41335cfe3ce429c5e217508b8c427563a33
zaki <asdfwujian@126.com>
2015-02-16 17:56:36 +0800

    修改:         mysqlback/backmysql.py 	修改:         mysqlback/config/config.cfg 	新文件:       mysqlback/mydumper-0.6.2-1.el6.x86_64.rpm

--- mysqlback/backmysql.py
+++ mysqlback/backmysql.py
@@ -19,9 +19,8 @@ try:
         long_query_guard=config.get("backmysql","long_query_guard")
         kill_long_queries=config.getboolean("backmysql","kill_long_queries")
         snapshot_interval=config.get("backmysql","snapshot_interval")
-        snapshot_interval_time=config.get("backmysql","snapshot_interval_time")
+        snapshot_interval_time=config.getint("backmysql","snapshot_interval_time")
         no_locks=config.get("backmysql","no_locks")
-        TIME_ZONE=config.get("backmysql","TIME_ZONE")
         verbose=config.getint("backmysql","verbose")
         lock_all_tables=config.get("backmysql","lock_all_tables")
         logfile=config.get("backmysql","logfile")
@@ -29,6 +28,33 @@ try:
 except :
     print sys.exc_info()[0],":",sys.exc_info()[1]
     sys.exit()
+if kill_long_queries == "True":
+    kill_long_queries="--kill-long-queries"
+else:
+    kill_long_queries=""
+if snapshot_interval=="True":
+    snapshot_interval="--snapshot-interval "+repr(snapshot_interval_time)
+else:
+    snapshot_interval=""
+if no_locks == "True":
+    no_locks="--no-locks "
+else:
+    no_locks=""
+if lock_all_tables == "True":
+    lock_all_tables="--lock-all-tables"
+else:
+    lock_all_tables=""
+if logfile == "True":
+    logfile="-L "+logfile_path
+else:
+    logfile=""
+if verbose != 0 or 1 or 2 or 3:
+    verbose="--verbose "+repr(verbose)
+else:
+    print "verbose must 0,1,2,3"
+    sys.exit()
+
+
 
 try:
     conn=MySQLdb.connect(host=dbhost,user=username,passwd=password,db=dbname,port=port) 
@@ -38,38 +64,20 @@ try:
     full_table=[]
     for i in full_value:
         full_table.append(i[0])
-    print full_table
-    #exclude_table=r'^users*'
     back_table=''
     exclude_table_re=set(exclude_table_re.split(","))
-    #print exclude_table_re
-    #print exclude_table_re
-    #a=r"%s" % exclude_table_re
-    #print a 
     
     for exclude in exclude_table_re:
         back_table=''
         for table in full_table:
             if not re.findall(r"%s" % exclude,table):
-                #if not re.findall(r"%s" % table,back_table):
                 back_table=(table+","+back_table).strip(",")
-                #print back_table
-                #else:
-                #   pass
             else:
                 pass
         full_table=back_table.split(",")
     cur.close()
     conn.close()
-    print back_table
 except MySQLdb.Error,e:
     print "Mysql Error %d: %s" % (e.args[0],e.args[1])
     sys.exit()
-commands.getstatusoutput("mydumper -B %s -u %s -p %s -P %s  -h %s -o %s -T %s -r %s -D -t %s" % (dbname,username,password,port,dbhost,outputdir,back_table,rows,threads))
-
-
-
-
-
-
-
+print "mydumper -B %s -u %s -p %s -P %s  -h %s -o %s -T %s -r %s -D -t %s %s %s %s %s %s %s" % (dbname,username,password,port,dbhost,outputdir,back_table,rows,threads,kill_long_queries,snapshot_interval,no_locks,lock_all_tables,verbose,logfile)
